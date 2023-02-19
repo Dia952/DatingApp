26e00d00b3624fe0462b46799de08833342e8f76
Dia952 <dia.sabri95@gmail.com>
2023-02-19 11:50:51 +0200

    Extended the API:
        * Extended User Entity
        * Added a DateTime extension to calculate age
        * Entity Framework Relationships
        * Generating seed Data
        * Added Repository Pattern
        * Created Repository
        * Updated the User Controller
        * Added a DTO for Members
        * Added and Configured AutoMapper
        * Used AutoMapper queryable extensions

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using API.Data;
 using API.Interfaces;
 using API.Services;
@@ -10,10 +11,10 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddDbContext<DataContext>(options => options.UseSqlite(config.GetConnectionString("DefaultConnection")));
-
             services.AddCors();
-
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             return services;
         }
